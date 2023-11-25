ad0f3d324e253929a9ce6c539dc150df3f45916e
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-11-25 00:07:37 +0000

    test

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#this to create a file /tmp
+file {'/tmp/school':
+ensure  => file,
+owner   => www-data,
+group   => www-data,
+mode    => '0744',
+content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+THis is puppet task
