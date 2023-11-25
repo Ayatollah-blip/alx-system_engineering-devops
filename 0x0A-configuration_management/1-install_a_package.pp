87175b2b2f181414752e9c47bee09dd3afeca2bd
Aymane El yamani <aymaneelyamani3@gmail.com>
2023-11-25 00:34:38 +0000

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #!/usr/bin/pup
-#this to create a file /tmp
+
+# Install an especific version of flask (2.1.0)
+
 package {'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+
+  ensure   => '2.1.0',
+
+  provider => 'pip3'
+
 }
